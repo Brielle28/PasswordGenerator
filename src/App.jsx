2364810949f86935007cc7f4957b5accb877ce3f
Brielle28 <chukwuemerieclara@gmail.com>
2024-10-24 17:57:19 +0100

    initial set up of password generator

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PasswordGenerator from "./Pages/PasswordGenerator";
+
+function App() {
+  return (
+    <>
+      <PasswordGenerator />
+    </>
+  );
+}
+
+export default App;
