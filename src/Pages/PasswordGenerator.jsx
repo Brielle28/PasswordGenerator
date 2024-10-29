d38731ec0ffd21e1e169aebdd790bdd68b0b5e9b
Brielle28 <chukwuemerieclara@gmail.com>
2024-10-29 12:00:00 +0000

    functionality

--- src/Pages/PasswordGenerator.jsx
+++ src/Pages/PasswordGenerator.jsx
@@ -50,6 +50,15 @@ export const PasswordGenerator = () => {
     setPassword(generatedPassword); // Update password state
   };
 
+  const getPasswordStrength = () => {
+    const activeOptions = Object.values(checkboxes).filter(Boolean).length;
+    if (activeOptions === 4) return "Very Strong";
+    if (activeOptions === 3) return "Strong";
+    if (activeOptions === 2) return "Medium";
+    if (activeOptions === 1) return "Weak";
+    return "";
+  };
+
   return (
     <div className="bg-[#100F15] h-screen flex flex-col items-center justify-center px-4 sm:px-8">
       <h1 className="text-[#817F8D] font-NotoSans font-bold mb-3">
@@ -70,13 +79,14 @@ export const PasswordGenerator = () => {
         </h1>
       </div>
       <div className="w-full sm:w-[80%] md:w-[50%] lg:w-[26%] flex-col bg-[#24232B] px-6 py-6 mt-4 flex items-start justify-start font-IBMPlexMono">
+        
         {/* Slider */}
         <div className="flex flex-col items-center justify-center w-full">
           <div className="flex items-center justify-between w-full mb-7">
-            <h1 className="text-[13px] text-white font-IBM Plex Mono">
+            <h1 className="text-[13px] text-white font-IBMPlexMono">
               Character Length
             </h1>
-            <span className="text-[17px] text-[#A4FFAF] font-IBM Plex Mono">
+            <span className="text-[17px] text-[#A4FFAF] font-IBMPlexMono">
               {value}
             </span>
           </div>
@@ -96,96 +106,45 @@ export const PasswordGenerator = () => {
         <div className="w-full mt-10 ml-1">
           <div className="bg-[#24232B] w-full text-white">
             <div className="flex flex-col space-y-4">
-              {/* Uppercase */}
-              <label className="flex items-center space-x-3">
-                <input
-                  type="checkbox"
-                  name="uppercase"
-                  checked={checkboxes.uppercase}
-                  onChange={handleCheckboxChange}
-                  className="checkbox border-white outline-0 rounded-[3px] [--chkbg:#A4FFAF] [--chkfg:black] checked:border-green-500"
-                />
-                <span
-                  className={`${
-                    checkboxes.uppercase === true
-                      ? "text-[#A4FFAF]"
-                      : "text-white"
-                  } text-[12px] md:text-sm`}
-                >
-                  Include Uppercase Letters
-                </span>
-              </label>
-              {/* Lowercase */}
-              <label className="flex items-center space-x-3">
-                <input
-                  type="checkbox"
-                  name="lowercase"
-                  checked={checkboxes.lowercase}
-                  onChange={handleCheckboxChange}
-                  className="checkbox border-white outline-0 rounded-[3px] [--chkbg:#A4FFAF] [--chkfg:black] checked:border-green-500"
-                />
-                <span
-                  className={`${
-                    checkboxes.lowercase === true
-                      ? "text-[#A4FFAF]"
-                      : "text-white"
-                  } text-[12px] md:text-sm`}
-                >
-                  Include Lowercase Letters
-                </span>
-              </label>
-              {/* Numbers */}
-              <label className="flex items-center space-x-3">
-                <input
-                  type="checkbox"
-                  name="numbers"
-                  checked={checkboxes.numbers}
-                  onChange={handleCheckboxChange}
-                  className="checkbox border-white outline-0 rounded-[3px] [--chkbg:#A4FFAF] [--chkfg:black] checked:border-green-500"
-                />
-                <span
-                  className={`${
-                    checkboxes.numbers === true
-                      ? "text-[#A4FFAF]"
-                      : "text-white"
-                  } text-[12px] md:text-sm`}
-                >
-                  Include Numbers
-                </span>
-              </label>
-              {/* Symbols */}
-              <label className="flex items-center space-x-3">
-                <input
-                  type="checkbox"
-                  name="symbols"
-                  checked={checkboxes.symbols}
-                  onChange={handleCheckboxChange}
-                  className="checkbox border-white outline-0 rounded-[3px] [--chkbg:#A4FFAF] [--chkfg:black] checked:border-green-500"
-                />
-                <span
-                  className={`${
-                    checkboxes.symbols === true
-                      ? "text-[#A4FFAF]"
-                      : "text-white"
-                  } text-[12px] md:text-sm`}
-                >
-                  Include Symbols
-                </span>
-              </label>
+              {["uppercase", "lowercase", "numbers", "symbols"].map((option) => (
+                <label key={option} className="flex items-center space-x-3">
+                  <input
+                    type="checkbox"
+                    name={option}
+                    checked={checkboxes[option]}
+                    onChange={handleCheckboxChange}
+                    className="checkbox border-white outline-0 rounded-[3px] [--chkbg:#A4FFAF] [--chkfg:black] checked:border-green-500"
+                  />
+                  <span
+                    className={`${
+                      checkboxes[option] ? "text-[#A4FFAF]" : "text-white"
+                    } text-[12px] md:text-sm`}
+                  >
+                    Include {option.charAt(0).toUpperCase() + option.slice(1)}
+                  </span>
+                </label>
+              ))}
             </div>
           </div>
         </div>
 
         {/* Strength Showcase */}
         <div className="flex items-center justify-between w-full bg-[#18171F] mt-5 p-3">
-          <h1 className="font-medium text-gray-200 font-IBMPlexMono">
-            Strength
-          </h1>
+          <h1 className="font-medium text-gray-200 font-IBMPlexMono">Strength</h1>
+          <p className="font-medium text-[#A4FFAF] font-IBMPlexMono">
+            {getPasswordStrength()}
+          </p>
           <div className="flex items-center justify-center gap-1">
-            <div className="w-2 h-6 font-bold border-2 border-white font-IBMPlexMono" />
-            <div className="w-2 h-6 font-bold border-2 border-white font-IBMPlexMono" />
-            <div className="w-2 h-6 font-bold border-2 border-white font-IBMPlexMono" />
-            <div className="w-2 h-6 font-bold border-2 border-white font-IBMPlexMono" />
+            {Array.from({ length: 4 }, (_, i) => (
+              <div
+                key={i}
+                className={`w-2 h-6 font-bold ${
+                  i < Object.values(checkboxes).filter(Boolean).length
+                    ? "bg-orange-600 border-2 border-orange-600"
+                    : "border-2 border-white"
+                }`}
+              />
+            ))}
           </div>
         </div>
 
